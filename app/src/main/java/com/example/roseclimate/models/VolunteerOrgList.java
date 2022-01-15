e88b0630f0b186f7e88f42e6ddd7d4b1d5cd385c
Goodfeller <ghothi8@students.cs.ubc.ca>
2022-01-15 14:26:24 -0800

    VolunteerOrg list object

---
+++ app/src/main/java/com/example/roseclimate/models/VolunteerOrgList.java
@@ -0,0 +1,36 @@
+package com.example.roseclimate.models;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class VolunteerOrgList implements Iterable<VolunteerOrg>{
+    private List<VolunteerOrg> volunteerOrgList;
+
+    public VolunteerOrgList() {
+        volunteerOrgList = new ArrayList<>();
+    }
+
+    public List<VolunteerOrg> getVolunteerOrgList() {
+        return volunteerOrgList;
+    }
+
+    public List<VolunteerOrg> sortByLocation(String location) {
+        List<VolunteerOrg> locationList = new ArrayList<>();
+        for (VolunteerOrg org: volunteerOrgList) {
+            if (org.getLocation() == location) {
+                locationList.add(org);
+            }
+        }
+        return locationList;
+
+    }
+
+    @NonNull
+    @Override
+    public Iterator<VolunteerOrg> iterator() {
+        return volunteerOrgList.iterator();
+    }
+}
